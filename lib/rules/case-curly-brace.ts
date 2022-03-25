24ed2b0395eb9efb47d2e25e4b70a89565f3ab5d
Black-Hole <158blackhole@gmail.com>
2022-03-25 15:28:35 +0800

    Revert "chore: upgrade eslint to 8 (#9)"
    This reverts commit 2b5dda89669b9f40aa85e219139a8eedcb523d5a.

--- lib/rules/case-curly-brace.ts
+++ lib/rules/case-curly-brace.ts
@@ -7,6 +7,7 @@ export = createRule({
 
         docs: {
             description: "when using case, be sure to use it with curly braces",
+            category: "Best Practices",
             recommended: "error",
         },
 
